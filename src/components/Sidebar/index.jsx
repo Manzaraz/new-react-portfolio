8bba9a8b92f55aebbb189cd5343684d66817c735
manzaraz <magnusmanz@gmaild.com>
2022-07-07 10:14:53 -0300

    home annimatedLetters

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '.././../assets/images/logo-s.png'
-import LogoSubtitle from '.././../assets/images/logo_sub.png'
+import LogoS from '.././../assets/images/1logo-s.png'
+import LogoSubtitle from '.././../assets/images/1logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import {
@@ -9,12 +9,14 @@ import {
   faInstagram,
   faLinkedin,
   faTwitter,
+  faWhatsapp,
 } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
+        {/* <Logo /> */}
         <img src={LogoS} alt="logo" />
         <img src={LogoSubtitle} alt="Manzaraz" className="sub-logo" />
       </Link>
@@ -74,6 +76,15 @@ const Sidebar = () => {
             rel="noreferrer"
           >
             <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://wa.me/5493815091639?text=Hola%20Chris!%20Vi%20tu%20portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faWhatsapp} color="#4d4d4e" />
           </a>
         </li>
       </ul>
